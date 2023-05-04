af3cc856b0823d90b90ca13ab5924a82ba3bd0c3
kawsarhamidi <kawsarhamidi77@gmail.com>
2023-05-04 22:18:58 +0600

    add to data

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-const chef = require('./data/chefdata.json');
-
+const chef = require('./data/chefFoods.json');
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Welcome')
-})
-
-app.get('/chef/', (req, res) =>{
+app.get('/', (req, res)=> {
+    res.send('welcome')
+});
+app.get('/chefdetails', (req, res)=> {
     res.send(chef)
+});
+
+app.get('/chefdetails/:id', (req, res)=> {
+    const id = req.params.id;
+    const selectedChef = chef.find(chef => chef.id === id);
+    request.send(selectedChef); 
 })
 
-app.listen(port, ()=>{
-    console.log(`Hello World: ${port}`);
+app.listen(port, () =>{
+    console.log(`listed port${port}`);
 })
